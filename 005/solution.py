5c8b5b4a4ac51c91ac2bff913bd56c2934c0f2f4
Joshua Shanks <jjshanks@gmail.com>
2013-08-14 09:24:53 +0000

    make python solution for problem 5 generic

--- 005/solution.py
+++ 005/solution.py
@@ -2,13 +2,23 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+max_value = 20
+
+# return the product of the values to the powers of the same index
 def applyPowers(values, powers):
   total = 1
   for (v,m) in zip(values, powers):
     total *= v ** m
   return total
 
-primes = [2,3,5,7,11,13,17,19]
-powers = [4,2,1,1, 1, 1, 1, 1]
+# increment power values till they would otherwise exceed the max number you want to divide by (max_value)
+def incrementPowers(values, powers, max_value):
+  for index, value in enumerate(values):
+    while values[index] ** (powers[index] + 1) < max_value:
+      powers[index] += 1
 
+primes = [2,3,5,7,11,13,17,19]
+powers = [1] * len(primes)
+
+incrementPowers(primes, powers, max_value)
 print applyPowers(primes, powers)
