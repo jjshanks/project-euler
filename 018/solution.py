9458e1c9bf26cbf4cb97b24f1f9b4b4a8e985123
Joshua Shanks <jjshanks@gmail.com>
2013-09-04 05:27:13 +0000

    fix comments

--- 018/solution.py
+++ 018/solution.py
@@ -3,6 +3,7 @@
 import networkx as nx
 from dijkstra import *
 
+# populate "tree"
 tree = []
 tree.append([75])
 tree.append([95,64])
@@ -24,22 +25,27 @@ graph = nx.DiGraph()
 node = 1
 
 graph.add_node(1, value=tree[0][0])
+# convert "tree" into graph
 for i in xrange(0,len(tree)-1):
     top_row = tree[i]
     bottom_row = tree[i+1]
     for pointer in xrange(0, len(top_row)):
         graph.add_node(node + i + 1, value=bottom_row[pointer])
         graph.add_node(node + i + 2, value=bottom_row[pointer+1])
+        # max value is less than 100 so weights are 100-value
         graph.add_edge(node, node + i + 1, weight=100-bottom_row[pointer])
         graph.add_edge(node, node + i + 2, weight=100-bottom_row[pointer+1])
         node += 1
 
+# add length of last row to node coun since we don't go over it
 node += len(tree[-1]) - 1
 biggest = 0
 
+# find the shortest path for each "leaf"
 for x in tree[-1]:
     path = shortest_path(graph, 1, node)
     total = 0
+    # for path find total
     for y in path:
         total += graph.node[y]['value']
     if total > biggest:
