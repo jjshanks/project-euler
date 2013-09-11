630abd58c9f900b237288400f3535876ee9f6cf6
Joshua Shanks <jjshanks@gmail.com>
2013-09-11 04:41:40 +0000

    add lex permutation method to helper file and update extra solution for 15 to use it

--- 015/path_printer.py
+++ 015/path_printer.py
@@ -6,8 +6,12 @@
 # 2) Find the largest index l such that a[k] < a[l].
 # 3) Swap the value of a[k] with that of a[l].
 # 4) Reverse the sequence from a[k + 1] up to and including the final element a[n].
+import os, sys, inspect
+cmd_folder = os.path.dirname(os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
 
-import sys
+from euler_helpers import *
 
 size = 3
 if len(sys.argv) >= 2:
@@ -17,24 +21,7 @@ path = list("d" * size + "r" * size)
 
 found = True
 
-while found:
-    found = False
-    for k in xrange(len(path)-2, -1, -1):
-        # step 1
-        if path[k] < path[k+1]:
-            for l in xrange(len(path)-1, -1, -1):
-                # step 2
-                if path[k] < path[l]:
-                    print path
-                    found = True
-                    # step 3
-                    tmp = path[k]
-                    path[k] = path[l]
-                    path[l] = tmp
-                    # step 4
-                    path = path[0:k + 1] + path[-1:k:-1]
-                    break
-            if found:
-                break
+while path != None:
+    print path
+    path = lex_permut(path)
 
-print path
--- euler_helpers.py
+++ euler_helpers.py
@@ -12,3 +12,24 @@ def sum_divisors(num):
         total -= root
     return total - num
 
+# From http://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+# 1) Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+# 2) Find the largest index l such that a[k] < a[l].
+# 3) Swap the value of a[k] with that of a[l].
+# 4) Reverse the sequence from a[k + 1] up to and including the final element a[n].
+#
+# Returns a permutated list unless there are no permutations left then None is returned
+def lex_permut(chars):
+    for k in xrange(len(chars)-2, -1, -1):
+        # step 1
+        if chars[k] < chars[k+1]:
+            for l in xrange(len(chars)-1, -1, -1):
+                # step 2
+                if chars[k] < chars[l]:
+                    # step 3
+                    tmp = chars[k]
+                    chars[k] = chars[l]
+                    chars[l] = tmp
+                    # step 4
+                    return chars[0:k + 1] + chars[-1:k:-1]
+    return None
