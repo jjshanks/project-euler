daf466d347e6e84d8dde86435073f2ba7dc87ad4
Joshua Shanks <jjshanks@gmail.com>
2013-09-13 04:06:17 +0000

    python solution for problem 25

---
+++ 025/solution.py
@@ -0,0 +1,39 @@
+# https://projecteuler.net/problem=25
+#
+# The Fibonacci sequence is defined by the recurrence relation:
+#
+# Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
+# Hence the first 12 terms will be:
+#
+# F1 = 1
+# F2 = 1
+# F3 = 2
+# F4 = 3
+# F5 = 5
+# F6 = 8
+# F7 = 13
+# F8 = 21
+# F9 = 34
+# F10 = 55
+# F11 = 89
+# F12 = 144
+# The 12th term, F12, is the first term to contain three digits.
+#
+# What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+import os, sys, inspect
+cmd_folder = os.path.dirname(os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0])))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+from euler_helpers import *
+
+a = b = 1
+term = 2
+while 1000 > len(str(a)):
+    tmp = a
+    a = a + b
+    b = tmp
+    term += 1
+
+print term
